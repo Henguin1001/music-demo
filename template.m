44bcfa05ccb98a1880875cd5b1f7a722d4a7308f
Henry Troutman <henguin1001@gmail.com>
2017-12-26 19:46:02 -0800

    removing files

--- template.m
+++ template.m
@@ -1,6 +1,6 @@
 <template name="pi">pi</template>
 <template name="angular">2*<pi/>*{{attributes.f|default(1)}}</template>
-<template name="sine">sin(<angular f="{{attributes.f}}"/>*x)</template>
+<template name="sine">{{attributes.a? attributes.a ~ '*':'' }}sin(<angular f="{{attributes.f}}"/>*x)</template>
 <template name="clip">(x > {{attributes.start|default(0)}} & x < {{attributes.end}} ) .* {{j(element).text()}}</template>
 <template name="track">
 s = @(d)[0:1/8192:d];
